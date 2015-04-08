c7e573e9b60184ed7131b1b5c26f3fe28ef457c1
vorou <vorou@skbkontur.ru>
2015-04-08 10:00:49 +0500

    04/08/2015 10:00:48

--- save.ps1
+++ save.ps1
@@ -1,13 +1,2 @@
-cd ~\config
-
-$files =
-    "$profile",
-    "~\.gitconfig",
-    "~\.gitignore",
-    "c:\bin\ConEmu\ConEmu.xml"
-
-foreach ($f in $files) {
-    cp $f
-}
 git cm "$(get-date)"
 git push
