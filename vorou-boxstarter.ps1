beef199a363065d371611a55f33c775e73bee496
vorou <vorou@skbkontur.ru>
2015-04-12 15:31:39 +0500

    04/12/2015 15:31:35

---
+++ vorou-boxstarter.ps1
@@ -0,0 +1,12 @@
+cup -y chocolatey
+cinst -y git -params '/GitOnlyOnPath'
+if (!(where.exe git)) { Invoke-Reboot }
+
+cd ~
+git clone https://github.com/vorou/config.git
+
+cinst -y ConEmu
+$conemuLink = "C:\Program Files\ConEmu\ConEmu.xml"
+$conemuConfig = resolve-path ~\config\ConEmu.xml
+rm $conemuLink
+cmd /c mklink $conemuLink $conemuConfig
