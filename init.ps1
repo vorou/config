bf048633f0e0dc37ae28c292d5abfba0b0c1d3de
vorou <vorou@skbkontur.ru>
2015-07-31 23:19:37 +0500

    07/31/2015 23:19:36

--- init.ps1
+++ init.ps1
@@ -1,1 +1,6 @@
-cmd /c mklink /d "C:\Users\vorou\AppData\Roaming\Sublime Text 3\Packages\User" "C:\Users\vorou\code\config\sublime-3-packages-user\"
+$config = resolve-path ~\code\config
+cmd /c mklink "C:\Program Files\ConEmu\ConEmu.xml" "$config\ConEmu.xml"
+cmd /c mklink $profile "$config\Microsoft.PowerShell_profile.ps1"
+cmd /c mklink "$home\documents\visual studio 2015\settings\CurrentSettings.vssettings" "$config\CurrentSettings.vssettings"
+cmd /c mklink "$home\AppData\Roaming\JetBrains\ReSharper\vAny\GlobalSettingsStorage.DotSettings" "$config\GlobalSettingsStorage.DotSettings"
+cmd /c mklink /d "$home\AppData\Roaming\Sublime Text 3\Packages\User" "$config\sublime-3-packages-user\"
