4bd39f9aa6033822453843cfbe00c8c416dbf73c
vorou <vorou@skbkontur.ru>
2015-07-31 23:35:28 +0500

    07/31/2015 23:35:27

--- init.ps1
+++ init.ps1
@@ -1,6 +1,11 @@
+write-host "about to init configs..."
+start-sleep -s 10
+
 $config = resolve-path ~\code\config
 cmd /c mklink "C:\Program Files\ConEmu\ConEmu.xml" "$config\ConEmu.xml"
 cmd /c mklink $profile "$config\Microsoft.PowerShell_profile.ps1"
 cmd /c mklink "$home\documents\visual studio 2015\settings\CurrentSettings.vssettings" "$config\CurrentSettings.vssettings"
 cmd /c mklink "$home\AppData\Roaming\JetBrains\ReSharper\vAny\GlobalSettingsStorage.DotSettings" "$config\GlobalSettingsStorage.DotSettings"
+cmd /c mklink "$home\.gitconfig" "$config\.gitconfig"
+cmd /c mklink "$home\.gitignore" "$config\.gitignore"
 cmd /c mklink /d "$home\AppData\Roaming\Sublime Text 3\Packages\User" "$config\sublime-3-packages-user\"
