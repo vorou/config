1af2c3f4f7d7b36911b9ed32c70d104190464ddb
vorou <vorou@skbkontur.ru>
2015-08-06 09:49:11 +0500

    moar+atom

--- init.ps1
+++ init.ps1
@@ -1,15 +1,18 @@
 $config = resolve-path ~\code\config
 
-#cmd /c mklink "$home\hosts" "C:\Windows\System32\drivers\etc\hosts"
+cmd /c mklink "$home\hosts" "C:\Windows\System32\drivers\etc\hosts"
 
-#rm "C:\Program Files\ConEmu\ConEmu.xml", $profile, "$home\.gitconfig", "$home\.gitignore"
-#rm -re -fo "$home\AppData\Roaming\Sublime Text 3\Packages\User"
+rm -re -fo "$home\.atom"
+cmd /c mklink /d "$home\.atom" "$config\.atom"
 
-#cmd /c mklink "C:\Program Files\ConEmu\ConEmu.xml" "$config\ConEmu.xml"
-#cmd /c mklink $profile "$config\Microsoft.PowerShell_profile.ps1"
-#cmd /c mklink "$home\.gitconfig" "$config\.gitconfig"
-#cmd /c mklink "$home\.gitignore" "$config\.gitignore"
-#cmd /c mklink /d "$home\AppData\Roaming\Sublime Text 3\Packages\User" "$config\sublime-3-packages-user\"
+rm "C:\Program Files\ConEmu\ConEmu.xml", $profile, "$home\.gitconfig", "$home\.gitignore"
+rm -re -fo "$home\AppData\Roaming\Sublime Text 3\Packages\User"
+
+cmd /c mklink "C:\Program Files\ConEmu\ConEmu.xml" "$config\ConEmu.xml"
+cmd /c mklink $profile "$config\Microsoft.PowerShell_profile.ps1"
+cmd /c mklink "$home\.gitconfig" "$config\.gitconfig"
+cmd /c mklink "$home\.gitignore" "$config\.gitignore"
+cmd /c mklink /d "$home\AppData\Roaming\Sublime Text 3\Packages\User" "$config\sublime-3-packages-user\"
 
 rm "$home\documents\visual studio 2015\settings\CurrentSettings.vssettings", "$home\AppData\Roaming\JetBrains\Shared\vAny\GlobalSettingsStorage.DotSettings"
 cmd /c mklink "$home\documents\visual studio 2015\settings\CurrentSettings.vssettings" "$config\CurrentSettings.vssettings"
