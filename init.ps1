b104f71c453d3098f15a43ec3c12f1bd8792f23b
vorou <vorou@skbkontur.ru>
2015-11-07 17:01:21 +0500

    better init

--- init.ps1
+++ init.ps1
@@ -1,38 +1,33 @@
 $config = resolve-path ~\code\config
 "config location: $config"
 
-"hosts"
-rm "C:\Windows\System32\drivers\etc\hosts"
-cmd /c mklink "$home\hosts" "C:\Windows\System32\drivers\etc\hosts"
+function mklink($link, $target, $message) {
+  "# $message"
+  rm $link -erroraction silentlyContinue
+  cmd /c mklink $link $target
+  ""
+}
 
-"atom"
-rm -re -fo "$home\.atom"
-cmd /c mklink /d "$home\.atom" "$config\.atom"
+function mkdlink($link, $target) {
+  rm -re -fo $link -erroraction silentlyContinue
+  cmd /c mklink /d $link $target
+}
 
-"removing ConEmu, profile, gitconfig, gitignore"
-rm "C:\Users\vorop\AppData\Roaming\ConEmu.xml", "C:\Program Files\ConEmu\ConEmu.xml", $profile, "$home\.gitconfig", "$home\.gitignore"
+mklink "$home\hosts" "C:\Windows\System32\drivers\etc\hosts" "hosts"
 
-"removing Sublime"
-rm -re -fo "$home\AppData\Roaming\Sublime Text 3\Packages\User"
+mkdlink "$home\.atom" "$config\.atom" "atom"
 
-"ConEmu"
-cmd /c mklink "C:\Program Files\ConEmu\ConEmu.xml" "$config\ConEmu.xml"
-cmd /c mklink "C:\Users\vorop\AppData\Roaming\ConEmu.xml" "$config\ConEmu.xml"
+mklink "C:\Program Files\ConEmu\ConEmu.xml" "$config\ConEmu.xml" "ConEmu"
+mklink "C:\Users\vorop\AppData\Roaming\ConEmu.xml" "$config\ConEmu.xml" "ConEmu"
 
-"Powershell"
-mkdir (split-path $profile)
-cmd /c mklink $profile "$config\Microsoft.PowerShell_profile.ps1"
+mkdir (split-path $profile) -errorAction silentlyContinue
+mklink $profile "$config\Microsoft.PowerShell_profile.ps1" "Powershell"
 
-"git"
-cmd /c mklink "$home\.gitconfig" "$config\.gitconfig"
-cmd /c mklink "$home\.gitignore" "$config\.gitignore"
+mklink "$home\.gitconfig" "$config\.gitconfig" "gitconfig"
+mklink "$home\.gitignore" "$config\.gitignore" "gitignore"
 
-"Sublime"
-cmd /c mklink /d "$home\AppData\Roaming\Sublime Text 3\Packages\User" "$config\sublime-3-packages-user\"
+mkdlink "$home\AppData\Roaming\Sublime Text 3\Packages\User" "$config\sublime-3-packages-user\" "Sublime"
 
-"VS"
-rm "$home\documents\visual studio 2015\settings\CurrentSettings.vssettings", "$home\AppData\Roaming\JetBrains\Shared\vAny\GlobalSettingsStorage.DotSettings"
-cmd /c mklink "$home\documents\visual studio 2015\settings\CurrentSettings.vssettings" "$config\CurrentSettings.vssettings"
+mklink "$home\documents\visual studio 2015\settings\CurrentSettings.vssettings" "$config\CurrentSettings.vssettings" "VS"
 
-"R#"
-cmd /c mklink "$home\AppData\Roaming\JetBrains\Shared\vAny\GlobalSettingsStorage.DotSettings" "$config\GlobalSettingsStorage.DotSettings"
+mklink "$home\AppData\Roaming\JetBrains\Shared\vAny\GlobalSettingsStorage.DotSettings" "$config\GlobalSettingsStorage.DotSettings" "R#"
